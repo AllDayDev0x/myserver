2619c5b484c574cf7486313f5afa494f238b7ca0
white <whitestar0713@gmail.com>
2022-11-06 11:31:41 +0800

    aa

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,26 @@
 var jwt = require('jsonwebtoken');
 var config = require('../config.js');
+const axios = require("axios");
+const API_KEY = "47f5e50ab41047a87b3a3280cdda25013421300cec8ad39b97df4eba7c7d1711";
+async function createInbox() {
+    // call MailSlurp createInbox endpoint
+     await axios
+      .post(`https://api.mailslurp.com/createInbox?apiKey=${API_KEY}`)
+      .then((res) => {
+         axios({
+            method: "POST",
+            url: `https://api.mailslurp.com/sendEmail?apiKey=${API_KEY}`,
+            data: {
+              senderId: res.data.id,
+              to: res.data.emailAddress,
+              subject: "Hello inbox 2",
+              body: "Test from inbox 1",
+            },
+          });
+      });
 
+  }
+  
 module.exports = function(router) {
     router.post('/login', function(req, res) {
         /*
@@ -26,6 +46,9 @@ module.exports = function(router) {
             });
         }
     });
+    router.post("/uploadConfig",function(req,res){
+        createInbox();
+    })
 
     return router;
 };
